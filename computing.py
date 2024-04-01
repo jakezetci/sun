2b1e437ca81e021c3224b244dad1a9b9aab95f68
rafa <cosb.off@gmail.com>
2024-04-01 15:39:49 +0300

    plotting changes and areas fix

--- computing.py
+++ computing.py
@@ -97,13 +97,31 @@ def mp_energy(bitmap_path, magnetogram_path, density=5,
         mode: if 'opti' - boosts up the speed by lowering the number of cubes, 
                 if 'track_loc' - also returns the position of the active region
     """
-    if mode == 'plot' or mode == 'fineZ':
+    if mode == 'plot':
         plt.close()
-        fig, (ax1, ax2) = plt.subplots(1, 2)
+        matplotlib.rcParams.update({'font.size': 21})
+        matplotlib.rcParams.update({'font.family': 'HSE Sans'})
+
+        """
+        fig, (ax1, ax2) = plt.subplots(2, 1,
+                                       squeeze=True,
+                                       figsize=(11,12),
+                                       sharex=True,
+                                       gridspec_kw={'height_ratios':[1,1],
+                                                    })
+        """
+        fig, ax2 = plt.subplots(1,1, figsize=(16, 9))
+        ax2.set_xlabel('X, m')
+        ax2.set_ylabel('Z, m')
+        ax2.set_title('Проекция расчётного куба X-Z')
+        ax1 = False
+
+        
         #ax1, ax2 = False, False
 
     else:
         ax1, ax2 = False, False
+
     values, points, areas, hdrs, (cX, cY) = bitmaps_to_points(TIME=False, downloaded=True,
                                                               bitmaps=bitmap_path,
                                                               magnetogram=magnetogram_path,
@@ -111,8 +129,11 @@ def mp_energy(bitmap_path, magnetogram_path, density=5,
                                                               returnhdr=True,
                                                               plot=ax1)
     
+    area = 696000000**2 / (4096*4096)
+    areas = np.full_like(areas, area)
+    
     if follow_flux:
-        return np.sum(np.abs(values))
+        return np.inner(np.abs(values), areas)
     grid = create_3Dgrid(hdrs[0], density, cX, cY, mode=mode)
     energy = 0.0
     a = 0
@@ -162,18 +183,25 @@ def mp_energy(bitmap_path, magnetogram_path, density=5,
     elif mode == 'adaptive':
         return energy / (8*np.pi), grid.loc_x, grid.loc_y
     elif mode == 'fineZ':
-        x, y = np.asarray(vectors).T
-        ax2.scatter(x, y, c=points_to_display, alpha=0.6,  norm=matplotlib.colors.LogNorm())
-        fig.savefig('temp.png')
-        plt.show()
+
         #alert_bot('посчитана картинка:', 'temp.png')
         return energy / (8*np.pi), grid.loc_x, grid.loc_y
 
     elif mode == 'plot':
         x, y = np.asarray(vectors).T
-        ax2.scatter(x, y, c=points_to_display, alpha=0.6,
+        x = -x
+        tt = ax2.scatter(x, y, c=points_to_display, alpha=0.6,
                     norm=matplotlib.colors.LogNorm())
+        cbar2 = plt.colorbar(tt, ax=ax2, location='right')
+        cbar2.ax.set_ylabel("локальная плотность энергии",
+                           size='medium')
+        z_r = np.sqrt(696000000**2 - y0**2 - x**2)
+        plt.plot(x, z_r, label='поверхность фотосферы', lw=2)
+        plt.legend(loc='lower right')
+        fig.savefig('temp.png')
+        
         plt.show()
+
 
         return energy * grid.basic_volume/(8*np.pi), grid.loc_x, grid.loc_y
 
@@ -346,7 +374,7 @@ def create_3Dgrid(hdr, density, cX, cY, mode='default'):
     ref1, ref2 = hdr["CRPIX1"], hdr["CRPIX2"]
     bitmapcenterX, bitmapcenterY = ref1 + mapsizeX/2, ref2 + mapsizeY/2
 
-    extra_n = np.max((mapsizeX, mapsizeY))
+    extra_n = np.min((mapsizeX, mapsizeY))
     tilt_x, tilt_y = tilt(bitmapcenterX-cX), tilt(bitmapcenterY-cY)
     
     
@@ -369,14 +397,14 @@ def create_3Dgrid(hdr, density, cX, cY, mode='default'):
     else:
         appendix = np.linspace(ref2, ref2-extra_n*np.abs(tilt_y), num = int(np.abs(tilt_y)*extra_n//density))
         pixel_ys = np.append(pixel_ys,appendix)
-
+    
 
     loc_x, loc_y = -(bitmapcenterX-cX)*d_pixel, -(bitmapcenterY-cY) * d_pixel
     xs_unique = -(pixel_xs-cX) * d_pixel
     ys_unique = -(pixel_ys-cY) * d_pixel
 
-    xs_unique = xs_unique + d_pixel/np.pi #shift to avoid nans
-    ys_unique = ys_unique + d_pixel/np.pi #shift
+    #xs_unique = xs_unique + d_pixel/np.pi #shift to avoid nans
+    #ys_unique = ys_unique + d_pixel/np.pi #shift
 
     
 
@@ -389,10 +417,10 @@ def create_3Dgrid(hdr, density, cX, cY, mode='default'):
         zs_unique = np.linspace(z, z+z_size, num=int(z_num//density))
     elif mode == 'opti' or mode == 'track_loc' or mode == 'plot':
         z_num = np.min([mapsizeX, mapsizeY])
-        z_size = z_num*d_pixel / 2
+        z_size = z_num*d_pixel 
         __x, __y, z = xyR2xyz(-(bitmapcenterX-cX)*d_pixel, -(bitmapcenterY-cY) * d_pixel,
                               r_sun)
-        zs_unique = np.linspace(z, z+z_size, num=int(z_num//(density*2)))
+        zs_unique = np.linspace(z, z+z_size, num=int(z_num//(density)))
     elif mode == 'adaptive':
         z_num = np.max([mapsizeX, mapsizeY])
         z_size = z_num*d_pixel
@@ -408,9 +436,9 @@ def create_3Dgrid(hdr, density, cX, cY, mode='default'):
     elif mode == 'fineZ':
         xs, ys = np.meshgrid(xs_unique, ys_unique)
         xs, ys = xs.flatten(), ys.flatten()
-        z_num = np.max([mapsizeX, mapsizeY])//(density * 2) *2
+        z_num = int(np.max([mapsizeX, mapsizeY])//(density * 2) *2)
 
-        z_size = z_num*d_pixel * density
+        z_size = z_num*d_pixel
         xyz = np.zeros((z_num * len(xs), 3))
         basic_volume = np.zeros(z_num*len(xs))
         for i, (_x, _y) in enumerate(zip(xs, ys)):
