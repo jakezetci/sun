461b26436ca77215fee85266237ff2d349259476
rafael <cosb.off@gmail.com>
2024-04-08 15:11:29 +0300

    name conception

--- test-newfunc.py
+++ test-newfunc.py
@@ -17,27 +17,41 @@ import os
 '''2011-02-11T00:00:00.000000000
 '''
 "04.09.2017 - 08.09.2017"
-N = 100  # количество временных точек
-density = 5  # пикселей на один объёмный куб
+alerts = True
+
+
+N = 16  # количество временных точек
+density = 4  # пикселей на один объёмный куб
 day = 13  # начальная дата
 year = 2011
 month = '02'
 noaa_ar = 11158  # номер активной области
-frequency = '1h'  # частота расчётов h - hour min - minute; данные приходят раз в 12 минут
+frequency = '6h'  # частота расчётов h - hour min - minute; данные приходят раз в 12 минут
 start_time = '00:00:00'
 home_path = os.path.expanduser("~") + '\\sunpy\\data'
+
+energy_fname = f'energy_{noaa_ar}_dens={density}_const.txt'
+date_fname = f'dates_{noaa_ar}_dens{density}_const.txt'
+loc_fname = f'locs_{noaa_ar}_dens{density}_const.txt'
 
 
 if __name__ == '__main__':
 
+    #dates_total = pd.date_range(start=f"{year}-{month}-{day} {start_time}",
+    #                      freq=frequency, periods=N).values
+    #
+    #dates_old = np.loadtxt(
+    #            f'dates_{noaa_ar}_{day}_dens{density}_const.txt', dtype=np.datetime64)
+    #
+    #dates = np.setdiff1d(dates_total, dates_old)
     dates = pd.date_range(start=f"{year}-{month}-{day} {start_time}",
                           freq=frequency, periods=N).values
 
     np.savetxt(
-        f'dates_{noaa_ar}_{day}_dens{density}_curv.txt', dates, fmt='%s')
+        date_fname, dates, fmt='%s')
     try:
         energys = np.loadtxt(
-            f'energys_{noaa_ar}_density={density}, day={day}_curv.txt')
+            energy_fname)
     except:
         energys = []
     print(energys)
@@ -64,36 +78,36 @@ if __name__ == '__main__':
                 date, 'hmi.m_720s', general_path=home_path)]
         except FileNotFoundError:
             dts = np.loadtxt(
-                f'dates_{noaa_ar}_{day}_dens{density}_curv.txt', dtype=np.datetime64)
-            np.savetxt(f'dates_{noaa_ar}_{day}_dens{density}_curv.txt', np.delete(
+                date_fname, dtype=np.datetime64)
+            np.savetxt(date_fname, np.delete(
                 dts, i - failed_counter), fmt='%s')
             failed_counter += 1
             continue
 
         energy, x, y = computing.mp_energy(bitmap_path, magnetogram_path, density=density,
-                                           onlyactive=False, mode='fineZ', threads=10,
+                                           onlyactive=True, mode='fineZ', 
                                            follow_flux=False)
         
         
         print(f'{date} - {energy}')
         energys = np.append(energys, energy)
         #continue
-        computing.alert_bot(f'{date} - {energy}')
+        if alerts:
+            computing.alert_bot(f'{date} - {energy}')
 
-        energys = np.append(energys, energy)
         xs.append(x)
         ys.append(y)
         xy = np.vstack([xs, ys]).T
 
         np.savetxt(
-            f'energys_{noaa_ar}_density={density}, day={day}_curv.txt', energys)
+            energy_fname, energys)
         np.savetxt(
-            f'locs_{noaa_ar}_density={density}, day={day}_curv.txt', xy)
+            loc_fname, xy)
 
     print(f'{failed_counter} downloads failed')
     fig, ax = plots.config(xlabel='date', ylabel='enegry, erg')
     dts = np.loadtxt(
-        f'dates_{noaa_ar}_{day}_dens{density}_curv.txt', dtype=np.datetime64)
+        date_fname, dtype=np.datetime64)
 
     ax.plot(dts, energys, 'o'
             )
